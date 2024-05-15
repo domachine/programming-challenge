7ccae1c86a24fd3a65481bc3efbe146cfc4c62b2
Dominik Burgdörfer <db@burgdoerfer.com>
2024-05-15 16:19:42 +0200

    Initial draft version
    This is the first draft of the solution accompanied with a very broad scoped testsuite. It helps me to refactor the code before extension.

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,25 +1,107 @@
 package de.exxcellent.challenge;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 /**
- * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
+ * The entry class for your solution. This class is only aimed as starting point
+ * and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 public final class App {
-
     /**
      * This is the main entry method of your program.
+     * 
      * @param args The CLI arguments passed
+     * @throws IOException
      */
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
+        List<Day> weatherData = readWeatherData("weather.csv");
+        Day dayWithSmallestTempSpread = getDayWithSmallestTempSpread(weatherData);
 
-        // Your preparation code …
+        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread.getNumber());
+    }
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+    /**
+     * This method contains the algorithm to find the day with the smallest
+     * temperature spread in a list of days.
+     * 
+     * @param days
+     * @return
+     */
+    static Day getDayWithSmallestTempSpread(List<Day> days) {
+        Day dayWithSmallestTempSpread = null;
+        int min = Integer.MAX_VALUE;
+        for (Day day : days) {
+            if (day.getTemperatureSpread() < min) {
+                min = day.getTemperatureSpread();
+                dayWithSmallestTempSpread = day;
+            }
+        }
+        return dayWithSmallestTempSpread;
+    }
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+    /**
+     * This method is responsible for reading the input csv an turning it into a
+     * list of days. It is probably better placed in a separate "Driver" class. But
+     * for the moment I leave it here for simplicity reasons.
+     *
+     * @param fileName
+     * @return
+     * @throws IOException
+     */
+    static List<Day> readWeatherData(String fileName) throws IOException {
+        InputStream is = App.class.getResourceAsStream(fileName);
+        ArrayList<Day> days = new ArrayList<>();
+
+        CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
+                .setHeader(WeatherDataHeader.class)
+                .setSkipHeaderRecord(true)
+                .build();
+
+        try (Reader reader = new InputStreamReader(is)) {
+            Iterable<CSVRecord> records = csvFormat.parse(reader);
+            for (CSVRecord record : records) {
+                String day = record.get(WeatherDataHeader.name);
+                String mxt = record.get(WeatherDataHeader.mxt);
+                String mnt = record.get(WeatherDataHeader.mnt);
+                days.add(new Day(day, Integer.parseInt(mxt), Integer.parseInt(mnt)));
+            }
+        }
+
+        return days;
+    }
+}
+
+enum WeatherDataHeader {
+    name, mxt, mnt
+}
+
+class Day {
+    private String number;
+    private int mxt;
+    private int mnt;
+
+    Day(String number, int mxt, int mnt) {
+        this.number = number;
+        this.mxt = mxt;
+        this.mnt = mnt;
+    }
+
+    int getTemperatureSpread() {
+        return mxt - mnt;
+    }
+
+    String getNumber() {
+        return number;
     }
 }
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,31 +1,55 @@
 package de.exxcellent.challenge;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 
 /**
  * Example JUnit 5 test case.
+ * 
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
+    @TestFactory
+    Stream<DynamicTest> weatherCsvTests2() {
+        List<WeatherTestCase> testCases = Arrays.asList(
+                new WeatherTestCase("weather.csv", "14"),
+                new WeatherTestCase("weather_sample_2.csv", "2"));
 
-    private String successLabel = "not successful";
+        return testCases.stream().map(testCase -> DynamicTest.dynamicTest(testCase.getFile(), () -> {
+            Day day = App.getDayWithSmallestTempSpread(App.readWeatherData(testCase.getFile()));
+            assertEquals(testCase.getExpected(), day.getNumber());
+        }));
+    }
+}
 
-    @BeforeEach
-    void setUp() {
-        successLabel = "successful";
+/**
+ * This is a tiny class to hold information about a test case. I like it to have
+ * multiple chunks of sample data in a format which is consumed by the
+ * application (in this case csv). So it's probably more an integration test
+ * than a unit test. But
+ * I think this makes the test more robust against further refactorings of the
+ * code.
+ */
+class WeatherTestCase {
+    private String file;
+    private String expected;
+
+    WeatherTestCase(String file, String expected) {
+        this.file = file;
+        this.expected = expected;
     }
 
-    @Test
-    void aPointlessTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
+    String getFile() {
+        return this.file;
     }
 
-    @Test
-    void runFootball() {
-        App.main("--football", "football.csv");
+    String getExpected() {
+        return this.expected;
     }
-
 }
