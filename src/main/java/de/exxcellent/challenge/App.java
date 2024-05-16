90545f2af098fc92d0936b93edfc4aaf2a23fc0d
Dominik Burgdörfer <db@burgdoerfer.com>
2024-05-16 07:16:21 +0200

    Improve command line interface

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,50 +1,66 @@
 package de.exxcellent.challenge;
 
-import java.util.Optional;
+import java.io.IOException;
+import java.io.InputStream;
 
 import de.exxcellent.challenge.football.FootballCsvReader;
-import de.exxcellent.challenge.football.FootballResult;
 import de.exxcellent.challenge.football.FootballResultsAnalyzer;
-import de.exxcellent.challenge.weather.Day;
 import de.exxcellent.challenge.weather.WeatherAnalyzer;
 import de.exxcellent.challenge.weather.WeatherCsvReader;
 
 /**
- * This is the main entry class of the program. Since there are no requirements
- * specified how the output should look like I keep this rather simple and
- * extensible. Means throwing exceptions instead of outputting nicely formatted
- * error messages.
+ * This is the main entry class of the program.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 public final class App {
     /**
-     * This is the main entry method of your program.
+     * This is the main entry method of the program.
      * 
      * @param args The CLI arguments passed
      * @throws Exception
      */
     public static void main(String... args) throws Exception {
         if (args.length != 2) {
-            throw new Exception("invalid number of arguments");
+            System.err.printf("invalid number of arguments%n%nusage: --weather file.csv | --football file.csv%n");
+            System.exit(1);
         }
-        if (args[0].equals("--weather")) {
-            WeatherAnalyzer analyzer = new WeatherAnalyzer(
-                    new WeatherCsvReader(App.class.getResourceAsStream(args[1])));
-            Optional<Day> dayWithSmallestTempSpread = analyzer.getDayWithSmallestTempSpread();
-            System.out.printf("Day with smallest temperature spread : %s%n",
-                    dayWithSmallestTempSpread.orElseThrow(() -> new Exception("no lines from input received"))
-                            .getNumber());
-        } else if (args[0].equals("--football")) {
-            FootballResultsAnalyzer analyzer = new FootballResultsAnalyzer(
-                    new FootballCsvReader(App.class.getResourceAsStream(args[1])));
-            Optional<FootballResult> footballResultWithSmallestGoalSpread = analyzer
-                    .getFootballResultWithSmallestGoalDifference();
-            System.out.printf("Team with smallest goal spread       : %s%n",
-                    footballResultWithSmallestGoalSpread
-                            .orElseThrow(() -> new Exception("no lines from input received")).getTeam());
-        } else {
-            throw new Exception("unknown processing mode\n\nusage: --weather file.csv | --football football.csv");
+        try {
+            if (args[0].equals("--weather")) {
+                new WeatherAnalyzer(
+                        new WeatherCsvReader(readResourceFile(args[1])))
+                        .getDayWithSmallestTempSpread()
+                        .ifPresentOrElse((day) -> {
+                            System.out.printf("day with smallest temperature spread : %s%n",
+                                    day.getNumber());
+                        }, () -> {
+                            System.out.println("no input lines");
+                        });
+            } else if (args[0].equals("--football")) {
+                new FootballResultsAnalyzer(
+                        new FootballCsvReader(readResourceFile(args[1])))
+                        .getFootballResultWithSmallestGoalDifference()
+                        .ifPresentOrElse((result) -> {
+                            System.out.printf("team with smallest goal spread       : %s%n",
+                                    result.getTeam());
+                        }, () -> {
+                            System.out.println("no input lines");
+                        });
+            } else {
+                System.err.printf("unknown processing mode%n%nusage: --weather file.csv | --football file.csv%n");
+                System.exit(1);
+            }
+        } catch (IOException e) {
+            System.err.printf("failed to read input source: %s%n", e.getMessage());
+            System.exit(1);
         }
+    }
+
+    private static InputStream readResourceFile(String fileName) throws IOException {
+        InputStream stream = App.class.getResourceAsStream(fileName);
+        if (stream == null) {
+            throw new IOException("resource file not found");
+        }
+        return stream;
     }
 }
